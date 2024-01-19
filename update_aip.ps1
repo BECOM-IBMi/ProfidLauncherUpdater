03647d854f8cb0974e486d65bcc00996afc91474
Michael Prattinger <michael.prattinger@becom-group.com>
2024-01-19 15:15:47 +0100

    Updateing the aip script

--- update_aip.ps1
+++ update_aip.ps1
@@ -1,26 +1,28 @@
 $assets = (get-location).path + "\assets"
-$outpf = (get-location).path + "\output"
+$outpf = (get-location).path + "\artifacts"
 $package = (get-location).path + "\installer"
 
 $aip = $assets + "\ProfidLauncher.aip"
 $icon = $assets + "\favicon.ico"
 
-cp $assets\*.ico $outpf\Assets\
-
 $advinst = New-Object -ComObject AdvancedInstaller
 
 $project = $advinst.LoadProject($aip)
 
-$tag = git tag
+$tag = git describe --abbrev=0
 $tag = $tag.Replace('v', '')
 $tag = $tag.Substring(0, $tag.LastIndexOf('.') + 1)
 
-$tag = $tag + $ENV:BUILD_NUMBER
+if($ENV:BUILD_NUMBER) {
+    $tag = $tag + $ENV:BUILD_NUMBER
+} else {
+    $tag = $tag + "99"
+}
 
 $project.ProductDetails.Version = $tag
 $project.ProductDetails.SetIcon($icon)
 
-$pl = $project.FilesComponent.FindFileByPath("APPDIR\ProfidLauncher.exe")
+$pl = $project.FilesComponent.FindFileByPath("APPDIR\ProfidLauncherUpdater.exe")
 
 $sc = $project.ShortcutsComponent.CreateFileShortcut($project.PredefinedFolders.Desktop, $pl)
 $sc.Icon($icon)
