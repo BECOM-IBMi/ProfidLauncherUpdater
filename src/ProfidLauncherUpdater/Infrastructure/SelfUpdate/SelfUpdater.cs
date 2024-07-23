42e2676c502aee3d9f7d800d547c196a937eda91
mprattinge <michael.prattinger@becom-group.com>
2024-07-23 14:07:14 +0200

    fixing msiexec parameter issues

--- src/ProfidLauncherUpdater/Infrastructure/SelfUpdate/SelfUpdater.cs
+++ src/ProfidLauncherUpdater/Infrastructure/SelfUpdate/SelfUpdater.cs
@@ -123,7 +123,8 @@ public class SelfUpdater
     {
         var proc = new Process();
         proc.StartInfo.FileName = "msiexec";
-        proc.StartInfo.Arguments = String.Format($"/i {msiFile} /passive");
+        proc.StartInfo.Arguments = String.Format($"/i \"{msiFile}\" /passive");
+
         proc.Start();
     }
 }
